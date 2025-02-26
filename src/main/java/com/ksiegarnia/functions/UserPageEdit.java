4d5c5a6d5b947a80a5c9ec4c6debce23cdeb81f7
Twiggiermaen21 <kacper.pudelko.kpk@gmail.com>
2025-02-26 16:04:56 +0100

    userPage update version 2 complete

--- src/main/java/com/ksiegarnia/functions/UserPageEdit.java
+++ src/main/java/com/ksiegarnia/functions/UserPageEdit.java
@@ -13,6 +13,7 @@ import jakarta.faces.context.FacesContext;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import jakarta.persistence.PersistenceException;
 import java.io.Serializable;
 import org.primefaces.event.CellEditEvent;
 import org.primefaces.event.RowEditEvent;
@@ -24,43 +25,54 @@ import org.primefaces.event.RowEditEvent;
 @Named
 @ViewScoped
 public class UserPageEdit implements Serializable {
-    private Uzytkownik user ;
-    
+
+    private Uzytkownik user;
+
     @Inject
-   UserData data;
+    UserData data;
+    @Inject
     UserDAO userDAO;
-       
-    
-    
-@PostConstruct
+
+    @PostConstruct
     public void init() {
         user = data.getUser();
-       
+
     }
-    public Uzytkownik  getuser() {
+
+    public Uzytkownik getuser() {
         return user;
     }
-public void setService(UserData service) {
+
+    public void setService(UserData service) {
         this.data = service;
     }
 
-    public void onRowEdit(RowEditEvent<Uzytkownik> event) {
-      try {
-    userDAO.merge(event.getObject());
-
-} catch (Exception e) {
-    // Inne, mniej specyficzne wyjątki
-    FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Unexpected Error", "Something went wrong.");
-    FacesContext.getCurrentInstance().addMessage(null, msg);
-}
-       
-        
-        FacesMessage msg = new FacesMessage("Product Edited", String.valueOf(event.getObject().getIdUzytkownik()));
+  public void onRowEdit(RowEditEvent<Uzytkownik> event) {
+    try {
+        Uzytkownik userr = (Uzytkownik) event.getObject();
+        if (userr != null) {
+            userDAO.merge(userr);  // Zatwierdzenie zmian
+            FacesMessage msg = new FacesMessage("User Edited", String.valueOf(userr.getEmail()));
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+        }
+    } catch (PersistenceException e) {
+        // Obsługa błędów związanych z bazą danych
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Database Error", "Something went wrong with the database.");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    } catch (Exception e) {
+        // Inne, mniej specyficzne wyjątki
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Unexpected Error", "Something went wrong.");
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
+}
 
+    
+    
+    
+    
+    
     public void onRowCancel(RowEditEvent<Uzytkownik> event) {
-        FacesMessage msg = new FacesMessage("Edit Cancelled", String.valueOf(event.getObject().getIdUzytkownik()));
+        FacesMessage msg = new FacesMessage("Edit Cancelled", String.valueOf(event.getObject().getEmail()));
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
 
@@ -71,8 +83,7 @@ public void setService(UserData service) {
         if (newValue != null && !newValue.equals(oldValue)) {
             FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed", "Old: " + oldValue + ", New:" + newValue);
             FacesContext.getCurrentInstance().addMessage(null, msg);
-            
-            
+
         }
     }
 }
