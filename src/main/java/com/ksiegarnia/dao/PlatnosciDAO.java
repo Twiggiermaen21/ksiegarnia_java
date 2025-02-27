95c1ceaddb739c6e743cfac9fa4916f00ec7a5a8
Twiggiermaen21 <kacper.pudelko.kpk@gmail.com>
2025-02-27 17:40:14 +0100

    busket page xhtml update 2

---
+++ src/main/java/com/ksiegarnia/dao/PlatnosciDAO.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ksiegarnia.dao;
+
+import com.ksiegarnia.entities.Platnosci;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author kacpe
+ */
+public class PlatnosciDAO {
+    private Platnosci platnosci;
+    private final static String UNIT_NAME = "jsfcourse-ksiegarniaPU";
+
+    // Dependency injection (no setter method is needed)
+    @PersistenceContext(unitName = UNIT_NAME)
+    protected EntityManager em;
+    
+    
+    
+    
+    public void create(Platnosci platnosci) {
+        em.persist(platnosci);
+    }
+
+   
+    
+
+
+}
