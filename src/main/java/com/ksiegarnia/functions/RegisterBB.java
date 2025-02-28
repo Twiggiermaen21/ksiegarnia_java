45133060be4a70792b5e090d0dd7b601ceb7258b
Twiggiermaen21 <kacper.pudelko.kpk@gmail.com>
2025-02-28 15:12:33 +0100

    new user complete

--- src/main/java/com/ksiegarnia/functions/RegisterBB.java
+++ src/main/java/com/ksiegarnia/functions/RegisterBB.java
@@ -4,6 +4,9 @@
  */
 package com.ksiegarnia.functions;
 
+import com.ksiegarnia.dao.UserDAO;
+import com.ksiegarnia.entities.Uzytkownik;
+import jakarta.ejb.EJB;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.FacesContext;
@@ -13,6 +16,7 @@ import jakarta.faces.context.FacesContext;
 import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 import java.io.Serializable;
+import java.util.Date;
 
 
 @Named
@@ -21,9 +25,24 @@ public class RegisterBB implements Serializable{
 
     private String username;
     private String surname;
+    private Date dataaktualizacji;
+     private int idaktualizacji;
     private String email;
     private String password;
-
+    private Date datautworzenia;
+    private Boolean aktywny;
+    
+    @EJB
+    private UserDAO userDAO;
+    
+       public RegisterBB() {
+           this.idaktualizacji=0;
+        this.dataaktualizacji= new Date(0);
+        this.datautworzenia = new Date(); // ustawia bieżącą datę i godzinę
+        this.aktywny = true; // Załóżmy, że domyślnie obiekt jest aktywny
+    }
+    
+    
     // Getters and Setters
     public String getUsername() {
         return username;
@@ -56,6 +75,8 @@ public class RegisterBB implements Serializable{
     public void setPassword(String password) {
         this.password = password;
     }
+    
+    
 
     // Metoda rejestracji
     public String register() {
@@ -63,8 +84,26 @@ public class RegisterBB implements Serializable{
         if (username != null && surname != null && email != null && password != null) {
             FacesContext.getCurrentInstance().addMessage(null,
                     new FacesMessage(FacesMessage.SEVERITY_INFO, "Rejestracja zakończona sukcesem!", null));
+            makeUser();
              return "login.xhtml?faces-redirect=true"; // lub inna strona, np. "home.xhtml"
         } 
       return null;
     }
+    
+    public String makeUser(){
+        Uzytkownik newUser=new Uzytkownik();
+        newUser.setEmail(email);
+        newUser.setHaslo(password);
+        newUser.setAktywny(aktywny);
+        newUser.setIdaktualizacji(idaktualizacji);
+        newUser.setDataaktualizacji(dataaktualizacji);
+        newUser.setDatautworzenia(datautworzenia);
+        newUser.setImie(username);
+        newUser.setNazwisko(surname);
+        userDAO.create(newUser);
+         return "succes";
+    }
+    
+    
+    
 }
