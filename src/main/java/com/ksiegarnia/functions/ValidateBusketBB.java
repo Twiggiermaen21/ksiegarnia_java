d7aef4f1fc58574e3ad0b4a2054bfce4ca9f3505
Twiggiermaen21 <kacper.pudelko.kpk@gmail.com>
2025-03-01 14:51:20 +0100

    validatebusketbb update adding fuctions for database

--- src/main/java/com/ksiegarnia/functions/ValidateBusketBB.java
+++ src/main/java/com/ksiegarnia/functions/ValidateBusketBB.java
@@ -4,8 +4,14 @@
  */
 package com.ksiegarnia.functions;
 
+import com.ksiegarnia.dao.PlatnosciDAO;
+import com.ksiegarnia.dao.ZamowieniaDAO;
 import com.ksiegarnia.entities.Ksiazki;
+import com.ksiegarnia.entities.Platnosci;
+import com.ksiegarnia.entities.Uzytkownik;
+import com.ksiegarnia.entities.Zamowienia;
 import com.ksiegarnia.enums.Busket;
+import jakarta.ejb.EJB;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.context.ExternalContext;
@@ -15,8 +21,12 @@ import jakarta.faces.validator.ValidatorException;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.servlet.http.HttpSession;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 @Named
 @RequestScoped
@@ -26,13 +36,22 @@ private static final String PAGE_BILL_SHOW = "billPage?faces-redirect=true";
     private Integer paymentMethod; // Metoda płatności
     private Boolean agree;
     private List<Busket> busket;
+    private Zamowienia zam;
+    private double totalPrice;
+    private int totalBooks;
     @Inject
     ExternalContext extcontext;
     @Inject
     Flash flash;
-
-    KsiazkiListBB KLBB;
-    
+    @EJB
+    ZamowieniaDAO zamowieniaDAO;
+    PlatnosciDAO platnoscDAO;
+                HttpSession session = (HttpSession) extcontext.getSession(false);
+private static final Map<String, String> paymentMethodMap = Map.of(
+        "0", "Przelew Bankowy",
+        "1", "Gotowka",
+        "2", "Blik"
+    );
     public Boolean getAgree() {
         return agree;
     }
@@ -54,6 +73,9 @@ private static final String PAGE_BILL_SHOW = "billPage?faces-redirect=true";
     public Integer getPaymentMethod() {
         return paymentMethod;
     }
+      public String getSelectedPaymentl() {
+        return paymentMethodMap.getOrDefault(paymentMethod, "Nieznana metoda");
+    }
 
     public void setPaymentMethod(Integer paymentMethod) {
         this.paymentMethod = paymentMethod;
@@ -66,13 +88,19 @@ private static final String PAGE_BILL_SHOW = "billPage?faces-redirect=true";
             fieldList.add(this.text);           // Add the 'text' value (address)
             fieldList.add(this.paymentMethod);  // Add the 'paymentMethod' value
             fieldList.add(this.agree);
-            HttpSession session = (HttpSession) extcontext.getSession(false);
 
             busket = (List<Busket>) session.getAttribute("busket");
+            for (Busket item : busket) {
+            totalPrice += item.getKsiazka().getCena() * item.getIlosc();
+            totalBooks += item.getIlosc();
+        }
 
             
               flash.put("busket", busket);
               flash.put("fieldList", fieldList);
+              
+              
+              
             removeBusket();
             // Możesz tutaj dodać logikę biznesową po zatwierdzeniu formularza
             FacesMessage msg = new FacesMessage("Formularz został wysłany");
@@ -101,11 +129,43 @@ private static final String PAGE_BILL_SHOW = "billPage?faces-redirect=true";
 }
     
     
-    public boolean zapisDoBazy(){
-        
+    public boolean GenerateZam(){
+        Zamowienia zam = new Zamowienia();
+                   
+
+        zam.setUzytkownikidUzytkownik( (Uzytkownik) session.getAttribute("user"));
+        zam.setDatazamowienia(new Date());
+        zam.setPlatnosciidPlatnosci(GeneratePlat());
+        zam.setAdres(text);
+        zam.setKodZamowienia(generateRandomCode());
+        zamowieniaDAO.create(zam);
         
         return true;
     }
     
+    public Platnosci GeneratePlat(){
+        Uzytkownik user=(Uzytkownik) session.getAttribute("user");
+        Platnosci pla = new Platnosci();
+        pla.setKwota( totalPrice);
+        pla.setDataPlatnosci(new Date());
+        pla.setRodzajpłatnosci(getSelectedPaymentl());
+        pla.setIdUzytkownik(user.getIdUzytkownik());
+        pla.setKodPlatnosci(generateRandomCode());
+        return pla;
+    }
+    
+    
+    
+    
+     public static String generateRandomCode() {
+        // Tworzenie losowego 16-znakowego ciągu alfanumerycznego
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder randomString = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < 16; i++) {
+            randomString.append(characters.charAt(random.nextInt(characters.length())));
+        }
+        return randomString.toString();
+    }
     
 }
